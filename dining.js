ce7de39e42d943c8bd000d03d86d740e666f8665
Virsandhu <80320001+Virsandhu@users.noreply.github.com>
2022-05-03 15:57:27 +0530

    Add files via upload

---
+++ dining.js
@@ -0,0 +1,54 @@
+objects=[];
+function preload(){
+    img= loadImage("dining.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+
+    objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status= true;
+    
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+        
+    }
+        console.log(results);
+        objects=results;
+    }
+    
+
+
+function draw(){
+    image(img,0,0,380,380);
+    
+    if(status!=""){
+        r= random(255);
+        g= random(255);
+        b= random(255);
+        objectDetector.detect(img,gotResult);
+        for(i=0;i<objects.length;i++){
+            
+            document.getElementById("status").innerHTML="Status: Object detected";
+            document.getElementById("number_of_objects_detected").innerHTML= "Number Of Objects Detected = "+ objects.length;
+
+            fill(r,g,b);
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y,objects[i].width, objects[i].height);
+        }
+    }
+
+}
+
+
